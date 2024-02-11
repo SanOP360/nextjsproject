00c3108f475e7ff1b4a9f9d92263465541626630
SanOP360 <sanjayl2001alm@gmail.com>
2024-02-11 12:40:55 +0530

    added a Link

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,5 +1,21 @@
+import teamDetails from "./teamDetials";
+import Link from "next/link";
+
 const AboutPage = () => {
-  return <h1>About Us</h1>;
+  return (
+    <>
+      <h1>About Us</h1>
+      <ul>
+        {teamDetails.map((person) => (
+          <li key={person.id}>
+            <Link href={`/aboutus/${person.id}`}>
+              {person.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default AboutPage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,16 @@
+import Link from 'next/link';
+
 const NewsPage = () => {
-  return <h1>News</h1>;
+  return (
+    <>
+      <h1>News</h1>
+      <ul>
+        <li>
+        <Link href='/news/greatframrwork'>Next js is a great framework</Link></li>
+        <li>Soemthing else</li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
